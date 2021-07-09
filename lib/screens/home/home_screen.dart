a8fa87711d7233377e6eea3c7df31fec8f38882a
FlutLab.io <>
2021-07-09 23:39:07 +0000

    Autosave

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -10,9 +10,7 @@ class HomeScreen extends StatelessWidget {
   FirebaseApp app;
 
   Future<void> initializeDefault() async {
-    app = await Firebase.app().whenComplete(() {
-      print(app);
-    });
+    app = await Firebase.app();
     assert(app != null);
     print("Getting");
     print('Initialized default app $app');
