03414cf91f3896e25041b01c59e625242b7aa6c3
FlutLab.io <>
2021-07-09 21:13:37 +0000

    Autosave

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -16,7 +16,7 @@ class HomeScreen extends StatelessWidget {
           ConstrainedBox(
             constraints: BoxConstraints(
               maxWidth: double.infinity,
-              maxHeight: double.infinity,
+              maxHeight: 50,
             ),
             child: UserAccountHeader(),
           ),
