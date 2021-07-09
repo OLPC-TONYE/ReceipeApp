1eb7d7f55ba7241b4a29f5fdfd7d48a35a418c1b
FlutLab.io <>
2021-07-09 02:00:17 +0000

    Autosave

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -29,7 +29,7 @@ class Body extends StatelessWidget {
                                     style: TextStyle(fontSize: 40.0),
                                     ),
                                 ),
-                            ),(),
+                            ),
         
       ]
     );
