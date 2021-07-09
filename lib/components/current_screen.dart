5ce222021b2e6799f8ffc3448bd3e2d7aaadd67a
FlutLab.io <>
2021-07-09 11:32:40 +0000

    Autosave

--- lib/components/current_screen.dart
+++ lib/components/current_screen.dart
@@ -4,7 +4,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class CurrentScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return _CurrentState();
+    return _CurrentScreenState();
   }
 }
 
