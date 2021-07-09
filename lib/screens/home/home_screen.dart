5880444737d9001f7b45060addbe34d74e0c1016
FlutLab.io <>
2021-07-09 20:34:16 +0000

    Autosave

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -19,7 +19,7 @@ class HomeScreen extends StatelessWidget {
           Icon(Icons.mood),
           Positioned(
             left: 20.0,
-            child: Icon(Icons.mood_mad),
+            child: Icon(Icons.mood),
           )
         ]),
       ),
