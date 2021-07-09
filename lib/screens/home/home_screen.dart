ea652eab6ba1bc616946afcbbebfa224a9b38107
FlutLab.io <>
2021-07-09 20:40:57 +0000

    Autosave

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -14,8 +14,9 @@ class HomeScreen extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.all(8.0),
         width: 100.0,
+        color: Colors.white,
         child: Stack(children: <Widget>[
-          FaIcon(FontAwesomeIcons.menu),
+          Icon(Icons.menu),
           Positioned(
             right: 20.0,
             child: Container(
