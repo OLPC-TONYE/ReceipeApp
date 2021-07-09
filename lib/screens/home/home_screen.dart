568e7d85aa729361ec46775e4f744ef66606f445
FlutLab.io <>
2021-07-09 01:44:13 +0000

    Autosave

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -5,7 +5,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        elevation: 0,
       ),
       body: Center(
         child: Text(
