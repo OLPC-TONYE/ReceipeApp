706631ec71fb3475107191f01003c3d9982b01b4
FlutLab.io <>
2021-07-09 11:04:11 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -23,35 +23,22 @@ class MyApp extends StatelessWidget {
             children: <Widget>[
               CurrentScreen(),
               Container(
-                margin: EdgeInsets.only(
-                  left: 30,
-                  top: 100,
-                  right: 30,
-                ),
-                padding: EdgeInsets.only(left: 35, right: 35),
+                margin: EdgeInsets.only(left: 30, top: 100, right: 30, bottom: 50),
+                height: double.infinity,
+                width: double.infinity,
                 decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10), bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10)),
                   boxShadow: [
                     BoxShadow(
                       color: Colors.grey.withOpacity(0.5),
                       spreadRadius: 5,
                       blurRadius: 7,
-                      offset: Offset(0, 3),
-                    )
-                  ],
-                  border: Border(
-                    top: BorderSide(width: 1.0, color: Colors.red),
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    FaIcon(FontAwesomeIcons.home),
-                    FaIcon(FontAwesomeIcons.plus),
-                    FaIcon(FontAwesomeIcons.heart),
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
                   ],
                 ),
-              ),
+              )
             ],
           ),
         ));
