62b83ade46533f8bdb8ff661279bdfab39f8238f
FlutLab.io <>
2021-07-09 22:52:06 +0000

    Autosave

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -22,6 +22,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print("Hello");
     initializeDefault();
     downloadURLExample();
     return Container(
