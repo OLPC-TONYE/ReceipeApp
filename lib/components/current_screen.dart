caa1241802c0817220de0e2b8747024d390c133a
FlutLab.io <>
2021-07-09 11:37:10 +0000

    Autosave

--- lib/components/current_screen.dart
+++ lib/components/current_screen.dart
@@ -16,16 +16,11 @@ class _CurrentScreenState extends State<CurrentScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('title'),
-      ),
-      body: Center(
-        child: Text(
-          'Text',
-        ),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16.0),
+        color: Colors.green,
       ),
     );
-    ;
   }
 }
