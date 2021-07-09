59c1e1cb842cfab1f10310e84568587bd2534e03
FlutLab.io <>
2021-07-09 11:15:22 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return MaterialApp(
         // Application name
         debugShowCheckedModeBanner: false,
