a1a22f0f59283b5a20f12c84bc392b0255ddef52
FlutLab.io <>
2021-07-09 11:22:47 +0000

    Autosave

--- lib/components/current_screen.dart
+++ lib/components/current_screen.dart
@@ -1,29 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class CurrentScreen extends StatefulWidget {
-  @override
-  _CurrentScreenState createState() => _CurrentScreenState();
-}
-
-class _CurrentScreenState extends State<CurrentScreen> {
-  @override
-  void initState() {
-    super.initState();
-    getScreen();
-  }
-
-  void getScreen() {
-    void setState() {}
-  }
-
+class CurrentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16.0),
-        color: Colors.green,
-      ),
-    );
+    return Container();
   }
 }
