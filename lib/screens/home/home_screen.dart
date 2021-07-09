de5cb2b07b24640813fabaf4db2f650f870971db
FlutLab.io <>
2021-07-09 02:11:51 +0000

    Autosave

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -19,17 +19,19 @@ class Body extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Container(
       child: Column(
-        UserAccountsDrawerHeader(
-                                accountName: Text("FlutLab User"),
-                                accountEmail: Text("flutlab.ide@gmail.com"),
-                                currentAccountPicture: CircleAvatar(
-                                    backgroundColor: Colors.white,
-                                    child: Text(
-                                    "F",
-                                    style: TextStyle(fontSize: 40.0),
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+            UserAccountsDrawerHeader(
+                                    accountName: Text("FlutLab User"),
+                                    accountEmail: Text("flutlab.ide@gmail.com"),
+                                    currentAccountPicture: CircleAvatar(
+                                        backgroundColor: Colors.white,
+                                        child: Text(
+                                        "F",
+                                        style: TextStyle(fontSize: 40.0),
+                                        ),
                                     ),
-                                ),
-                            ),
+                                ),,
         ],
       ),
     );
