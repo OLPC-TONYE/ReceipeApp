277f173630d53213cec49cc0aacf8d7ca11b3214
FlutLab.io <>
2021-07-10 22:26:34 +0000

    Autosave

--- lib/components/custom_widgets.dart
+++ lib/components/custom_widgets.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 class BottomAppNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
