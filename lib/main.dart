291988a6278315aea797ec294f0ba4241a4882b3
FlutLab.io <>
2021-07-10 22:15:49 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -21,41 +21,49 @@ class ReceipeApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
         primaryColor: Colors.red,
       ),
-      bottomNavigationBar: Container(
-        width: double.infinity,
-        padding: EdgeInsets.only(left: 20, right: 20, top: 10),
-        margin: EdgeInsets.only(
-          top: 10,
-        ),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 5,
-              blurRadius: 7,
-              offset: Offset(0, 3), // changes position of shadow
-            ),
-          ],
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            FaIcon(FontAwesomeIcons.home, size: 35),
-            FaIcon(FontAwesomeIcons.plus, size: 35),
-            FaIcon(FontAwesomeIcons.bookmark, size: 35),
-            FaIcon(FontAwesomeIcons.user, size: 35),
-          ],
-        ),
-      ),
+      bottomNavigationBar: BottomNavigationBar(),
       home: Scaffold(
         body: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
             CurrentScreen(),
+            BottomAppNavBar(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BottomAppNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+      margin: EdgeInsets.only(
+        top: 10,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          FaIcon(FontAwesomeIcons.home, size: 35),
+          FaIcon(FontAwesomeIcons.plus, size: 35),
+          FaIcon(FontAwesomeIcons.bookmark, size: 35),
+          FaIcon(FontAwesomeIcons.user, size: 35),
+        ],
       ),
     );
   }
