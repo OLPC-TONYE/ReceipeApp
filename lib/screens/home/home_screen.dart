c292f5f7642091b60989bc5df6240c7966c56b83
FlutLab.io <>
2021-07-10 20:50:07 +0000

    Autosave

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -50,7 +50,7 @@ class MostViewed extends StatelessWidget {
     return Container(
       padding: EdgeInsets.all(20),
       margin: EdgeInsets.only(top: 30),
-      width: MediaQuery.of(context).size.width,
+      width: MediaQuery.of(context).size.width * 0.85,
       decoration: BoxDecoration(
         image: DecorationImage(
           image: NetworkImage("https://firebasestorage.googleapis.com/v0/b/receipeapp-4dd90.appspot.com/o/assets%2Fimages%2Fff47aa505d388c4436579121b38e32ab.jpg?alt=media&token=9836f6d0-a551-4dfc-a153-4b98b9ea955b"),
