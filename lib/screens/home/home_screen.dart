7831fff6c430f3407db86cd4c935a25cc1655e68
FlutLab.io <>
2021-07-11 19:33:44 +0000

    Autosave

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -23,6 +23,7 @@ class HomeScreen extends StatelessWidget {
     return Column(
       children: <Widget>[
         Container(
+          height: MediaQuery.of(context).size.height * 0.285,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(16.0),
             color: Colors.green,
