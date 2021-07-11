c4a1f311452084dd1e642dee83b310590c7db6ec
FlutLab.io <>
2021-07-11 22:01:55 +0000

    Autosave

--- lib/components/main_screen.dart
+++ lib/components/main_screen.dart
@@ -34,6 +34,7 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: HomeScreen(),
       bottomNavigationBar: BottomAppNavBar(),
     );
