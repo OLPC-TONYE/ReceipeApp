7ec3aeed75ccc5019975528df19220a8fad7d497
FlutLab.io <>
2021-07-11 21:40:39 +0000

    Autosave

--- lib/screens/home/components.dart
+++ lib/screens/home/components.dart
@@ -15,10 +15,7 @@ class ReceipeButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(
-        top: 20,
-        left: 10,
-      ),
+      margin: EdgeInsets.only(top: 20, left: 10, right: 10),
       width: MediaQuery.of(context).size.width * 0.65,
       height: MediaQuery.of(context).size.height * 0.285,
       decoration: BoxDecoration(
