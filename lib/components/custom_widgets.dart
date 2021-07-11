88518a0192df2faade0ac0b9c6d7556c6680bfe2
FlutLab.io <>
2021-07-11 18:51:20 +0000

    Autosave

--- lib/components/custom_widgets.dart
+++ lib/components/custom_widgets.dart
@@ -26,7 +26,7 @@ class BottomAppNavBar extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
           IconButton(
-            icon: Image.asset("assets/icons/home.png", color: Colors.blue, scale: 40),
+            icon: Image.asset("assets/icons/home.png", color: Colors.blue, scale: 4),
             onPressed: () {},
           ),
           FaIcon(FontAwesomeIcons.plus, size: 35),
