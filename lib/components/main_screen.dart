d6a72591b892b41729ce9f18f8309e898a6446c0
FlutLab.io <>
2021-07-11 19:18:40 +0000

    Autosave

--- lib/components/main_screen.dart
+++ lib/components/main_screen.dart
@@ -40,7 +40,7 @@ class _MainScreenState extends State<MainScreen> {
           HomeScreen(),
         ],
       ),
-      bottomNavBar: BottomAppNavBar(),
+      bottomAppBar: BottomAppNavBar(),
     );
   }
 }
