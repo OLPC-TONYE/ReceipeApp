eb3c6cfae38333f2fcffebdfb33b6008cae2c0ee
FlutLab.io <>
2021-07-11 19:03:39 +0000

    Autosave

--- lib/components/custom_widgets.dart
+++ lib/components/custom_widgets.dart
@@ -55,7 +55,7 @@ class BottomAppNavBar extends StatelessWidget {
           IconButton(
             // icon: Image.asset("assets/icons/home.png", color: Colors.blue, width: 55, height: 55),
             icon: ImageIcon(
-              AssetImage("assets/icons/home.png"),
+              AssetImage("assets/icons/user2.png"),
               color: Colors.red,
               size: 135,
             ),
