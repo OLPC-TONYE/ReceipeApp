c85a39b2f7de7e8ba708751294901fbffef90524
FlutLab.io <>
2021-07-11 22:24:26 +0000

    Autosave

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -50,6 +50,7 @@ class HomeScreen extends StatelessWidget {
                 fontSize: 24,
               ),
             ),
+            Spacer()
             FlatButton(
               onPressed: () {},
               shape: RoundedRectangleBorder(
