76748b6aa1739ba46088fdfdb205619872bd1525
FlutLab.io <>
2021-07-12 03:59:37 +0000

    Autosave

--- lib/auth.dart
+++ lib/auth.dart
@@ -46,6 +46,25 @@ class AuthPage extends StatelessWidget {
                 ),
               ),
             ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(26.0),
+                color: Colors.white60,
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(left: 10, right: 10, top: 5),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Email e.g example@gmail.com',
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 10,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
