ebcdfe17608a3bc5de5769700ee663336cbda69a
FlutLab.io <>
2021-07-12 03:07:40 +0000

    Autosave

--- lib/components/main_screen.dart
+++ lib/components/main_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:firebase_core/firebase_core.dart';
