096c58de978eb4ecf758a29c64fa23e859e85896
FlutLab.io <>
2021-07-12 02:11:10 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,8 @@ import 'package:receipeapp/screens/home/home_screen.dart';
 import 'package:receipeapp/components/main_screen.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(ReceipeApp());
 }
 
