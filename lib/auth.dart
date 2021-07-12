4bd89e70ed9d6cad73c6385c422654e399b6919e
FlutLab.io <>
2021-07-12 19:17:21 +0000

    Autosave

--- lib/auth.dart
+++ lib/auth.dart
@@ -93,7 +93,21 @@ class _SignUpFormState extends State<SignUpForm> {
               if (_formKey.currentState.validate()) {
                 // If the form is valid, display a snackbar. In the real world,
                 // you'd often call a server or save the information in a database.
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(_usernameField.text + _passwordField.text)));
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Signing Up")));
+                try {
+                  UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                    email: "barry.allen@example.com",
+                    password: "SuperSecretPassword!",
+                  );
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'weak-password') {
+                    print('The password provided is too weak.');
+                  } else if (e.code == 'email-already-in-use') {
+                    print('The account already exists for that email.');
+                  }
+                } catch (e) {
+                  print(e);
+                }
               }
             },
             child: Text('Submit'),
