d8f2e5368c75ebef31e451f1df5c13ceecf7ed90
FlutLab.io <>
2021-07-12 18:24:14 +0000

    Autosave

--- lib/auth.dart
+++ lib/auth.dart
@@ -67,7 +67,7 @@ class _SignUpFormState extends State<SignUpForm> {
           ElevatedButton(
             onPressed: () {
               // Validate returns true if the form is valid, or false otherwise.
-              if (_formKey.currentState!.validate()) {
+              if (_formKey.currentState.validate()) {
                 // If the form is valid, display a snackbar. In the real world,
                 // you'd often call a server or save the information in a database.
                 ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Processing Data')));
