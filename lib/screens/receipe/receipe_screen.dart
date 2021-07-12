90ea3cc3bb258554de92c4f7dc6b7bb600f8c1d2
FlutLab.io <>
2021-07-12 01:14:58 +0000

    Autosave

--- lib/screens/receipe/receipe_screen.dart
+++ lib/screens/receipe/receipe_screen.dart
@@ -38,7 +38,7 @@ class ReceipeDetail extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         Container(
-          height: MediaQuery.of(context).size.height * 0.35,
+          height: MediaQuery.of(context).size.height * 0.40,
           width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
             image: DecorationImage(
